520b0258b14e7b712dd7330091fb9b5d44d4bde6
Kristian Serrano <kristianserrano@gmail.com>
2024-05-17 00:56:26 -0400

    Changes insertion of button to occur on chat log render

--- modules/swade-raise-calculator.mjs
+++ modules/swade-raise-calculator.mjs
@@ -14,7 +14,6 @@ Hooks.on('init', () => {
     });
 });
 
-
 Hooks.on('setup', () => {
     // Preload the template and render the UI
     loadTemplates([
@@ -22,16 +21,21 @@ Hooks.on('setup', () => {
     ]);
 });
 
-Hooks.on('ready', () => {
-    const rc = new RaiseCalculator();
-    const rcButton = document.createElement('a')
-    rcButton.setAttribute('aria-label', game.i18n.localize('SWADERaiseCalculator.OpenCalculatorButton.AltText'));
-    rcButton.setAttribute('role', 'button');
-    rcButton.setAttribute('data-tooltip', game.i18n.localize('SWADERaiseCalculator.OpenCalculatorButton.AltText'));
-    rcButton.classList.add('open-raise-calc');
-    const icon = document.createElement('i');
-    icon.classList.add('fas', 'fa-calculator');
-    rcButton.append(icon);
-    rcButton.addEventListener('click', () => rc.render(true));
-    document.querySelector('#chat-controls .chat-control-icon')?.insertAdjacentElement("afterend", rcButton);
-})
+Hooks.on('renderSwadeChatLog', (SwadeChatLog, html, options) => {
+    if (!ui.windows.SWADERaiseCalculator) {
+        ui.windows.SWADERaiseCalculator = new RaiseCalculator();
+    }
+
+    if (!html[0].querySelector('.open-raise-calc')) {
+        const rcButton = document.createElement('a');
+        rcButton.setAttribute('aria-label', game.i18n.localize('SWADERaiseCalculator.OpenCalculatorButton.AltText'));
+        rcButton.setAttribute('role', 'button');
+        rcButton.setAttribute('data-tooltip', game.i18n.localize('SWADERaiseCalculator.OpenCalculatorButton.AltText'));
+        rcButton.classList.add('open-raise-calc');
+        const icon = document.createElement('i');
+        icon.classList.add('fas', 'fa-calculator');
+        rcButton.append(icon);
+        rcButton.addEventListener('click', () => ui.windows.SWADERaiseCalculator.render(true));
+        html[0].querySelector('#chat-controls .chat-control-icon').insertAdjacentElement("afterend", rcButton);
+    }
+});
